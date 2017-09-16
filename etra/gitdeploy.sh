040d0953e8c8b883c5991c55faec82c6b27af1b4
ventas-dev <ventas.core.dev@gmail.com>
2017-09-16 13:39:30 +0900

    a

---
+++ etra/gitdeploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#if [ $# -lt 2 ]; then
+#    echo "usage gitdir srcdir "
+#    exit 0;
+#fi
+tarname=temp.tar
+# delete file
+rm -Rf $tarname
+orgdir=`pwd`
+gitdir=~/dev/blockchain/github/ventas-project/insight-api
+srcdir=./
+
+# copy to gitdir
+
+echo gitdir $gitdir
+echo srcdir $srcdir
+echo orgdir $orgdir
+
+tar cvf $tarname \
+    --exclude=data \
+    --exclude=.git \
+    --exclude=etra \
+    --exclude=tmp \
+    --exclude=depends/work \
+    --exclude=depends/built \
+    --exclude=depends/sources \
+    --exclude=depends/x86_64-apple-darwin11* \
+    --exclude=**.o \
+    --exclude=**.a \
+    --exclude=**.dmg \
+    --exclude=wallet.dat** \
+    --exclude=node_modules** \
+    --exclude=node_modules** \
+    --exclude=Ventas-Qt.app \
+    *
+
+rm -Rf $gitdir/*
+mv $tarname $gitdir
+cd $gitdir
+
+tar xvf $tarname
+rm $tarname
